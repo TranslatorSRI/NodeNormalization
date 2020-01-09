d0f0009cb1c54b72215b202ffd4b9d49753af94f
YaphetKG <kebedey@renci.org>
2020-01-09 12:09:02 -0500

    Reading compendum files, loading to redis.

--- src/load_redis.py
+++ src/load_redis.py
@@ -5,7 +5,7 @@ import json
 def get_config():
     cname = os.path.join(os.path.dirname(__file__),'..', 'config.json')
     with open(cname,'r') as json_file:
-        data = load(json_file)
+        data = json.load(json_file)
     return data
 
 def load_redis():
@@ -18,16 +18,16 @@ def load_redis():
 
 def get_compendia(config):
     """Return the list of compendum files to load"""
-    return []
+    return [os.path.join(config.get('compendium_directory'), file_name) for file_name in os.listdir(config.get('compendium_directory'))]
 
 def get_redis(config,dbid):
     """Return a redis instance"""
-    return redis.StrictRedis(host=config['redis_host'], 
+    return redis.StrictRedis(host=config['redis_host'],
                              port=int(config['redis_port']),
                              db = dbid,
                              password = config['redis_password'])
 
-def load_compendum(compendium_filename,config):
+def load_compendium(compendium_filename, config):
     """Given the full path to a compendium, load it into redis so that it can
     be read by R3.  We also load extra keys, which are the uppercased 
     identifiers, for ease of use"""
@@ -36,13 +36,24 @@ def load_compendum(compendium_filename,config):
     term2id_pipeline = term2id_redis.pipeline()
     id2instance_pipeline = id2instance_redis.pipeline()
     with open(compendium_filename,'r') as compendium:
+        print(f'Processing {compendium_filename}...')
         for line in compendium:
-            instance = json.reads(line)
+            instance = json.loads(line)
             identifier = instance['id']
             for equivalent_id in instance['equivalent_identifiers']:
                 #equivalent_id might be an array, where the first element is 
                 # the identifier, or it might just be a string. 
                 #Not implemented worrying about that yet.
-                term2id_pipeline.set(equivalent_id,identifier)
-                term2id_pipeline.set(equivalent_id.upper(),identifier)
+                if isinstance(equivalent_id, list):
+                    equivalent_id = equivalent_id[0]
+                term2id_pipeline.set(equivalent_id, identifier)
+                term2id_pipeline.set(equivalent_id.upper(), identifier)
             id2instance_pipeline.set(identifier, line)
+        print(f'Dumping to term2id db ...')
+        term2id_pipeline.execute()
+        print(f'Dumping to id2instance db ...')
+        id2instance_pipeline.execute()
+        print(f'Done loading {compendium_filename}...')
+
+if __name__ == '__main__':
+    load_redis()
