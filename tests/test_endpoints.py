4df5d12b088c81fe99dfeba5a6fe182e8e1e295f
Phil Owen <phillipsowen@outlook.com>
2020-07-24 10:29:05 -0400

    putting in test stubs

--- tests/test_endpoints.py
+++ tests/test_endpoints.py
@@ -3,13 +3,6 @@ from src.server import app
 
 def test_semantic_type_endpoint():
     pass
-    # print('Start: test_semantic_type_endpoint()')
-    #
-    # # make a good request
-    # request, response = app.test_client.post('/get_semantic_types')
-    #
-    # # was the request successful
-    # assert(response.status == 200)
 
 
 def test_curie_prefixes_endpoint():
