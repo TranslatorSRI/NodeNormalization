c3d14c14b6ef3fc4bf8aebb2122a1fe20942a08d
Phil Owen <phillipsowen@outlook.com>
2020-09-14 17:39:15 -0400

    insure the correct path to the test files

--- tests/test_nn.py
+++ tests/test_nn.py
@@ -1,19 +1,25 @@
 from src.NodeNormalization import NodeNormalization
+import os
 import pytest
-
 
 def test_nn_load():
     nn: NodeNormalization = NodeNormalization(True)
 
+    this_dir = os.path.dirname(os.path.realpath(__file__))
+
     nn._test_mode = 1
 
-    assert(nn.load_compendium("./tests/datafile.json", 5))
+    assert(nn.load_compendium(os.path.join(this_dir, "datafile.json"), 5))
 
 def test_nn_record_validation():
-    nn: NodeNormalization = NodeNormalization()
+    nn: NodeNormalization = NodeNormalization(True)
 
-    assert(nn.validate_compendia("./tests/datafile.json"))
+    this_dir = os.path.dirname(os.path.realpath(__file__))
 
-    ret_val = nn.validate_compendia("./tests/datafile_with_errors.json")
+    ret_val = nn.validate_compendia(os.path.join(this_dir, "datafile.json"))
 
-    assert(ret_val == False)
+    assert ret_val
+
+    ret_val = nn.validate_compendia(os.path.join(this_dir, "datafile_with_errors.json"))
+
+    assert(not ret_val)
