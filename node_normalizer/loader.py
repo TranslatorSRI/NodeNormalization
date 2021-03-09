35274fda2be541a24ae4ea816b739eb0e25907e2
Phil Owen <phillipsowen@outlook.com>
2021-03-09 13:52:52 -0500

    addition of functionality to convert the compendia files into KGX node/edge files.

--- node_normalizer/loader.py
+++ node_normalizer/loader.py
@@ -3,8 +3,10 @@ from itertools import islice
 from datetime import datetime
 from typing import Dict, Any
 import json
+import hashlib
+from itertools import combinations
 import jsonschema
-
+import os
 import redis
 
 
@@ -30,7 +32,7 @@ class NodeLoader:
         with open(json_schema) as json_file:
             self._validate_with = json.load(json_file)
 
-        # Initalize storage instance vars for the semantic types and source prefixes
+        # Initialize storage instance vars for the semantic types and source prefixes
         self.semantic_types: set = set()
         self.source_prefixes: Dict = {}
 
@@ -43,6 +45,135 @@ class NodeLoader:
             data = json.load(json_file)
 
         return data
+
+    def convert_to_kgx(self, outfile_name) -> bool:
+        """
+        Given a compendia directory, create a KGX node file
+        """
+
+        # init the return value
+        ret_val = True
+
+        line_counter: int = 0
+
+        try:
+            # get the list of files in the directory
+            compendia: list = self.get_compendia()
+
+            nodes: list = []
+            edges: list = []
+            pass_nodes: list = []
+
+            # did we get all the files
+            if len(compendia) == len(self._data_files):
+                # open the output file and start loading it
+                with open(os.path.join(self._compendium_directory, outfile_name + '_node.json'), 'w', encoding="utf-8") as node_file, \
+                     open(os.path.join(self._compendium_directory, outfile_name + '_edge.json'), 'w', encoding="utf-8") as edge_file:
+                    # start the output files
+                    node_file.write('{"nodes": [\n')
+                    edge_file.write('{"edges": [\n')
+
+                    # set the flag for suppressing the first ",\n" in the written data
+                    first = True
+
+                    # for each file validate and process
+                    for comp in compendia:
+                        # check the validity of the file
+                        if self.validate_compendia(comp):
+                            with open(comp, 'r', encoding="utf-8") as compendium:
+                                self.print_debug_msg(f'Processing {comp}...', True)
+
+                                # get the name of the source
+                                source = comp.parts[-1]
+
+                                # for each line in the file
+                                for line in compendium:
+                                    # increment the record counter
+                                    line_counter += 1
+
+                                    # clear storage for this pass
+                                    pass_nodes.clear()
+
+                                    # load the line into memory
+                                    instance: dict = json.loads(line)
+
+                                    # all ids (even the root one) are in the equivalent identifiers
+                                    if len(instance['equivalent_identifiers']) > 0:
+                                        # loop through each identifier and create a node
+                                        for equiv_id in instance['equivalent_identifiers']:
+                                            # check to see if there is a label. if there is use it
+                                            if 'label' in equiv_id:
+                                                name = equiv_id['label']
+                                            else:
+                                                name = ''
+
+                                            # add the node to the ones in this pass
+                                            pass_nodes.append({'id': equiv_id['identifier'], 'name': name, 'category': instance['type'], 'equivalent_identifiers': [x['identifier'] for x in instance['equivalent_identifiers']]})
+
+                                        # get the combinations of the nodes in this pass
+                                        combos = combinations(pass_nodes, 2)
+
+                                        # for all the node combinations create an edge between them
+                                        for c in combos:
+                                            # create a unique id
+                                            record_id: str = c[0]['id'] + 'biolink:same_as' + c[1]['id'] + f'NodeNormalizer:{comp}'
+
+                                            # save the edge
+                                            edges.append({'id': f'{hashlib.md5(record_id.encode("utf-8")).hexdigest()}', 'subject': c[0]['id'], 'predicate': 'biolink:same_as', 'object': c[1]['id'], 'source_database': f'NodeNormalizer:{source}'})
+
+                                    # save the nodes in this pass to the big list
+                                    nodes.extend(pass_nodes)
+
+                                    # did we reach the write threshold
+                                    if line_counter == 10000:
+                                        # first time in doesnt get a leading comma
+                                        if first:
+                                            prefix = ""
+                                        else:
+                                            prefix = ",\n"
+
+                                        # reset the first record flag
+                                        first = False
+
+                                        # reset the line counter for the next group
+                                        line_counter = 0
+
+                                        # get all the nodes in a string and write them out
+                                        nodes_to_write = prefix + ",\n".join([json.dumps(node) for node in nodes])
+                                        node_file.write(nodes_to_write)
+
+                                        # are there any edges to output
+                                        if len(edges) > 0:
+                                            # get all the edges in a string and write them out
+                                            edges_to_write = prefix + ",\n".join([json.dumps(edge) for edge in edges])
+                                            edge_file.write(edges_to_write)
+
+                                        # reset for the next group
+                                        nodes.clear()
+                                        edges.clear()
+
+                                # pick up any remainders in the file
+                                if len(nodes) > 0:
+                                    nodes_to_write = ",\n" + ",\n".join([json.dumps(node) for node in nodes])
+                                    node_file.write(nodes_to_write)
+
+                                if len(edges) > 0:
+                                    edges_to_write = ",\n" + ",\n".join([json.dumps(edge) for edge in edges])
+                                    edge_file.write(edges_to_write)
+                        else:
+                            self.print_debug_msg(f'Compendia file {comp} is invalid.', True)
+                            continue
+
+                    # finish off files
+                    node_file.write(']}')
+                    edge_file.write(']}')
+
+        except Exception as e:
+            self.print_debug_msg(f'Exception thrown in convert_to_KGX(): {e}', True)
+            ret_val = False
+
+        # return to the caller
+        return ret_val
 
     def load(self, block_size) -> bool:
         """
