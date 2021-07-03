8a21e1ed57afd793e562fbd6d6cf1da7e5cabfc0
Phil Owen <phillipsowen@outlook.com>
2021-07-03 12:34:23 -0400

    updating version to 1.1.0

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name='NodeNormalization',
-    version='1.0.0',
+    version='1.1.0',
     author='Patrick Wang',
     author_email='patrick@covar.com',
     url='https://github.com/patrickkwang/r3',
