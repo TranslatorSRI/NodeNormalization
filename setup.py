2eae5330941d9b8b11f7fdff2436d593aa6e2ed0
Phil Owen <phillipsowen@outlook.com>
2021-09-20 08:43:51 -0400

    upping trapi and app version

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name='NodeNormalization',
-    version='2.0.0',
+    version='2.0.1',
     author='Patrick Wang',
     author_email='patrick@covar.com',
     url='https://github.com/patrickkwang/r3',
