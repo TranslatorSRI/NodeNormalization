69a9794bf60a7f3b77553cb987791005767afac1
Chris Bizon <bizon@email.unc.edu>
2021-12-15 16:36:27 -0500

    A test verifying that issue #108 is fixed

--- tests/test_norm.py
+++ tests/test_norm.py
@@ -4,15 +4,26 @@ from node_normalizer.server import app
 from starlette.testclient import TestClient
 
 
+from unittest.mock import Mock, patch
+from helpers.redis_mocks import mock_get_equivalent_curies, mock_get_ic
+
+from pathlib import Path
+import os
+
 class MockRedis():
+    def __init__(self, data):
+        self.data = data
     async def mget(self, *args, **kwargs):
-        return [None for _ in args]
+        return [self.data[x]  if x in self.data else None for x in args ]
 
-
-app.state.redis_connection0 = MockRedis()
-app.state.redis_connection1 = MockRedis()
-app.state.redis_connection4 = MockRedis()
-
+import json
+app.state.redis_connection0 = MockRedis({"DOID:3812":"MONDO:0005002"})
+app.state.redis_connection1 = MockRedis({"MONDO:0005002":json.dumps([{"i":"MONDO:0005002"},{"i":"DOID:3812"}])})
+app.state.redis_connection2 = MockRedis({"MONDO:0005002":"biolink:Disease"})
+app.state.redis_connection3 = MockRedis({})
+app.state.redis_connection4 = MockRedis({})
+app.state.redis_connection5 = MockRedis({})
+app.state.ancestor_map={"biolink:Disease":["biolink:Disease","biolink:NamedThing"]}
 
 
 def test_not_found():
@@ -24,6 +35,13 @@ def test_not_found():
     result = json.loads(response.text)
     assert result == {"UNKNOWN:000000": None}
 
+def test_one_missing():
+    client = TestClient(app)
+    response = client.get('/get_normalized_nodes', params={"curie": ["UNKNOWN:000000", "DOID:3812"]})
+    result = json.loads(response.text)
+    assert len(result) == 2
+    assert result['UNKNOWN:000000'] == None
+    assert result['DOID:3812']['id']['identifier'] == 'MONDO:0005002'
 
 def test_empty():
     client = TestClient(app)
