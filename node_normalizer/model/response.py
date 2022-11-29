ba7630afda5806f98675a68fe3734d73e5fac0c6
Jason Reilly <jdr0887@gmail.com>
2022-11-29 11:38:23 -0500

    adding conflation type

--- node_normalizer/model/response.py
+++ node_normalizer/model/response.py
@@ -1,6 +1,7 @@
 """
 API Response Models not described in reasoner-pydantic
 """
+from enum import Enum
 
 from pydantic import BaseModel
 
@@ -11,21 +12,18 @@ class SemanticTypes(BaseModel):
     semantic_types: Dict[str, List]
 
     class Config:
-        schema_extra = {
-            "example": {
-                "semantic_types": {
-                    "types": [
-                        "cellular_component",
-                        "named_thing",
-                        "etc."
-                    ]
-                }
-            }
-        }
+        schema_extra = {"example": {"semantic_types": {"types": ["biolink:Cell", "biolink:AnatomicalEntity", "etc."]}}}
 
 
 class CuriePivot(BaseModel):
     curie_prefix: Dict[str, str]
 
+
 class ConflationList(BaseModel):
     conflations: List
+
+
+class ConflationType(str, Enum):
+    GENE_PROTEIN = "gene_protein"
+    CHEMICAL_DRUG = "chemical_drug"
+
