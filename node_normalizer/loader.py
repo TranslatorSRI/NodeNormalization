95e36cecef6fcbd1e23f953b834ecd7140379c7a
Yaphetkg <yaphetkg@renci.org>
2023-03-31 09:43:39 -0400

    bmt-lite fix get class uri relace with format

--- node_normalizer/loader.py
+++ node_normalizer/loader.py
@@ -11,6 +11,7 @@ import jsonschema
 import os
 from .redis_adapter import RedisConnectionFactory, RedisConnection
 from bmt import Toolkit
+from bmt.util import format as bmt_format
 
 from .util import LoggingUtil
 
@@ -49,7 +50,7 @@ class NodeLoader:
         if input_type in self.ancestor_map:
             return self.ancestor_map[input_type]
         a = self.toolkit.get_ancestors(input_type)
-        ancs = [self.toolkit.get_element(ai)["class_uri"] for ai in a]
+        ancs = [bmt_format(ai, case="pascal") for ai in a]
         if input_type not in ancs:
             ancs = [input_type] + ancs
         self.ancestor_map[input_type] = ancs
