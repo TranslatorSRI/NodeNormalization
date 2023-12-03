6cbcd25d1d51acfe1f57cb365c47b8e2461d10dd
Gaurav Vaidya <gaurav@renci.org>
2023-12-03 13:34:47 -0500

    Removed hardcoded TRAPI version, replaced with env var.
    Closes #215.

--- node_normalizer/apidocs.py
+++ node_normalizer/apidocs.py
@@ -71,7 +71,7 @@ def construct_open_api_schema(app) -> Dict[str, str]:
     if 'servers' in api_docs:
         for s in api_docs['servers']:
             # override if server root env var is provided
-            s['url'] = server_root + '1.4' if server_root != '/' else s['url']
+            s['url'] = server_root + os.environ.get("TRAPI_VERSION", "1.4") if server_root != '/' else s['url']
             s['x-maturity'] = os.environ.get("MATURITY_VALUE", "maturity")
             s['x-location'] = os.environ.get("LOCATION_VALUE", "location")
         open_api_schema['servers'] = api_docs['servers']
