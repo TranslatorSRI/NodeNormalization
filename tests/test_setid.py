2a3a7dbc6e9c5cc6fa499eb604755a7fe3361c4a
Gaurav Vaidya <gaurav@renci.org>
2024-07-11 11:48:33 -0400

    Added a basic setid test.

---
+++ tests/test_setid.py
@@ -0,0 +1,51 @@
+"""Test node_normalizer server.py"""
+import json
+from node_normalizer.server import app
+from fastapi.testclient import TestClient
+from unittest.mock import Mock, patch
+from .helpers.redis_mocks import mock_get_equivalent_curies, mock_get_ic
+from pathlib import Path
+import os
+from bmt import Toolkit
+
+
+class MockRedis:
+    def __init__(self, data):
+        self.data = data
+
+    async def mget(self, *args, **kwargs):
+        return [self.data[x] if x in self.data else None for x in args]
+
+
+# Id -> Canonical
+app.state.eq_id_to_id_db = MockRedis(
+    {"DOID:3812": "MONDO:0005002", "MONDO:0005002": "MONDO:0005002"}
+)
+# Canonical->Equiv
+app.state.id_to_eqids_db = MockRedis(
+    {"MONDO:0005002": json.dumps([{"i": "MONDO:0005002"}, {"i": "DOID:3812"}])}
+)
+app.state.id_to_type_db = MockRedis({"MONDO:0005002": "biolink:Disease"})
+app.state.curie_to_bl_type_db = MockRedis({})
+app.state.info_content_db = MockRedis({})
+app.state.gene_protein_db = MockRedis({})
+#app.state.ancestor_map = {"biolink:Disease": ["biolink:Disease", "biolink:NamedThing"]}
+app.state.toolkit = Toolkit()
+app.state.ancestor_map = {}
+
+
+def test_setid_empty():
+    client = TestClient(app)
+    response = client.get("/get_setid")
+    result = json.loads(response.text)
+    assert result == {
+        "detail":
+            [
+                {
+                    "loc": ["query", "curie"],
+                    "msg": "ensure this value has at least 1 items",
+                    "type": "value_error.list.min_items",
+                    "ctx": { "limit_value": 1 }
+                }
+            ]
+    }
